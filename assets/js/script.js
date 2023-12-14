774492d51d99ac5628a0ebb9679cb17b6349fcb0
SamMartin92 <samm92@live.ie>
2023-12-14 16:27:38 +0000

    adds selects to searchForm

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
 
 const searchForm = document.querySelector('form');
 let searchQuery = "";
+let mealType ="";
+let dishType="";
+let baseURL;
 
 const results = document.querySelector('#results');
 const btn = document.getElementById('btn');
 
 async function fetchAPI(){
-    const baseURL = `https://api.edamam.com/search?q=christmas+${searchQuery}&app_id=${APP_ID}&app_key=${APP_KEY}&to=20`
+    baseURL = `https://api.edamam.com/search?q=christmas+${searchQuery}&app_id=${APP_ID}&app_key=${APP_KEY}&to=20`
+    
+    if(mealType!=""){
+        baseURL+=`&mealType=${mealType}`
+    }
+
+    if(dishType!=""){
+        baseURL+=`&dishType=${dishType}`
+    }
+    
 
     const response = await fetch(baseURL);
     const data = await response.json();
@@ -14,16 +26,17 @@ async function fetchAPI(){
     for (let i=0; i<19;i++){
         showResults(data.hits[i]);
     }   
+    console.log(baseURL);
 
 }
 
 function showResults(searchResults){
     
-        results.innerHTML += `<div>
-        <p>${searchResults.recipe.label}<p>
-        <a href="${searchResults.recipe.url}" target ="_blank">Link<p>
-        <img src="${searchResults.recipe.image}"
-        </div>`;
+    results.innerHTML += `<div>
+    <p>${searchResults.recipe.label}<p>
+    <a href="${searchResults.recipe.url}" target ="_blank">Link<p>
+    <img src="${searchResults.recipe.image}"
+    </div>`;
     
 }
 
@@ -32,5 +45,8 @@ searchForm.addEventListener('submit', (e) =>{
     e.preventDefault();
     results.innerHTML='';
     searchQuery = e.target.querySelector('input').value;
+    mealType = e.target.querySelector('#mealType').value
+    dishType = e.target.querySelector('#dishType').value
+    console.log(dishType)
     fetchAPI();
 })
