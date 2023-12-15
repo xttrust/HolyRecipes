8cdbe7367765cfd57b567017c0ea225730b6cc54
SamMartin92 <samm92@live.ie>
2023-12-15 23:52:59 +0000

    wires fetchAPI to search in navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const searchForm = document.querySelector("#top-search-input");
+const searchForm = document.querySelector("form");
 let searchQuery = "";
 let mealType = "";
 let dishType = "";
@@ -25,9 +25,9 @@ async function fetchAPI() {
 
     results = data.hits;
 
-    for (let i = 0; i < results.length; i++) {
-        showResults(results[i]);
-    }
+    // for (let i = 0; i < results.length; i++) {
+    //     showResults(results[i]);
+    // }
 
     console.log(results)
 
@@ -74,10 +74,10 @@ function showResults(results) {
 
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    resultsHTML.innerHTML = '';
+    // resultsHTML.innerHTML = '';
     searchQuery = e.target.querySelector('input').value;
-    mealType = e.target.querySelector('#mealType').value;
-    dishType = e.target.querySelector('#dishType').value;
+    // mealType = e.target.querySelector('#mealType').value;
+    // dishType = e.target.querySelector('#dishType').value;
     fetchAPI();
 })
 
