21a146190fdcc8ab8364a14afa2dd7857b07fd6b
SamMartin92 <samm92@live.ie>
2023-12-15 23:28:53 +0000

    add config and api js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,82 @@
+const searchForm = document.querySelector('form');
+let searchQuery = "";
+let mealType = "";
+let dishType = "";
+let results;
+let baseURL;
 
-// const searchForm = document.querySelector('form');
-// let searchQuery = "";
-// let mealType ="";
-// let dishType="";
-// let baseURL;
+const resultsHTML = document.querySelector('#results');
+const btn = document.getElementById('btn');
 
-// const results = document.querySelector('#results');
-// const btn = document.getElementById('btn');
+async function fetchAPI() {
+    baseURL = `https://api.edamam.com/search?q=christmas+${searchQuery}&app_id=${APP_ID}&app_key=${APP_KEY}&to=20`
 
-// async function fetchAPI(){
-//     baseURL = `https://api.edamam.com/search?q=christmas+${searchQuery}&app_id=${APP_ID}&app_key=${APP_KEY}&to=20`
-    
-//     if(mealType!=""){
-//         baseURL+=`&mealType=${mealType}`
-//     }
+    if (mealType != "") {
+        baseURL += `&mealType=${mealType}`
+    }
 
-//     if(dishType!=""){
-//         baseURL+=`&dishType=${dishType}`
-//     }
-    
-
-//     const response = await fetch(baseURL);
-//     const data = await response.json();
-//     console.log(data.hits);
-//     for (let i=0; i<19;i++){
-//         showResults(data.hits[i]);
-//     }   
-//     console.log(baseURL);
-
-// }
-
-// function showResults(searchResults){
-    
-//     results.innerHTML += `<div>
-//     <p>${searchResults.recipe.label}<p>
-//     <a href="${searchResults.recipe.url}" target ="_blank">Link<p>
-//     <img src="${searchResults.recipe.image}"
-//     </div>`;
-    
-// }
+    if (dishType != "") {
+        baseURL += `&dishType=${dishType}`
+    }
 
 
-// searchForm.addEventListener('submit', (e) =>{
-//     e.preventDefault();
-//     results.innerHTML='';
-//     searchQuery = e.target.querySelector('input').value;
-//     mealType = e.target.querySelector('#mealType').value
-//     dishType = e.target.querySelector('#dishType').value
-//     console.log(dishType)
-//     fetchAPI();
-// })
+    const response = await fetch(baseURL);
+    const data = await response.json();
 
-$(document).ready(function() {
-    
-    $(window).scroll(function() {
-        // style when scrolling 
-        if ($(window).scrollTop() > 50) {
-            $('#navbar').removeClass('bg-transparent').addClass('bg-scroll')
-        // style when scrolled to top
-        } else {
-            $('#navbar').removeClass('bg-scroll').addClass('bg-transparent');
+    results = data.hits;
+
+    for (let i = 0; i < results.length; i++) {
+        showResults(results[i]);
+    }
+
+    console.log(results)
+
+
+}
+
+function showResults(results) {
+
+    let ingredientLines = "";
+    let instructionLines = "";
+    let ingredients = results.recipe.ingredientLines;
+    let instructions = results.recipe.instructionLines;
+
+    for (let i = 0; i < ingredients.length; i++) {
+        ingredientLines += `<li> ${ingredients[i]} </li>`
+    }
+
+
+
+    if (instructions && instructions.length > 0) {
+        for (let i = 0; i < instructions.length; i++) {
+            instructionLines += `<li> ${instructions[i]} </li>`;
         }
-    });
-});
 
+
+        resultsHTML.innerHTML += `<div>
+            <h2>${results.recipe.label}</h2>
+            <a href="${results.recipe.url}" target ="_blank">Link<p><a/>
+
+
+            <h3> 'Ingredient Line Values' </h3>
+            <ol>
+                ${ingredientLines}
+            </ol>
+            <h3> 'Instruction Line Values' </h3>
+            <ol>
+                ${instructionLines}
+            </ol>
+            <img src="${results.recipe.image}"
+        </div>`;
+    }
+}
+
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    resultsHTML.innerHTML = '';
+    searchQuery = e.target.querySelector('input').value;
+    mealType = e.target.querySelector('#mealType').value;
+    dishType = e.target.querySelector('#dishType').value;
+    fetchAPI();
+})
